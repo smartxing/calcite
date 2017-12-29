0af01c0c401aa3d9e28a6ebf90cdc5fe2908b0b5
xingliangbo <xingliangbo@u51.com>
2017-12-29 10:11:37 +0800

    init project

---
+++ src/main/java/com/liangbo/xing/calcite/CalciteApplication.java
@@ -0,0 +1,12 @@
+package com.liangbo.xing.calcite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalciteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalciteApplication.class, args);
+	}
+}
